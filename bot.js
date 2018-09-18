269b980c09ee858df8146c6bf7e829431ad7e200
lolsuperscratch <lolsuperscratch@hotmail.com>
2018-09-18 02:15:35 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,14 +4,36 @@ gClient.setChannel('lobby');
 gClient.start();
 var ex = 0;
 var ey = 0;
+var banned = [];
 var issweeping = false;
 setInterval(function (){ex = ex + 5;if (ex > 100){ex = -100; ey = Math.floor(Math.random() * 100)}gClient.moveMouse(ex,ey);gClient.setName('broom');},100);
-setInterval(function (){if (!issweeping){gClient.say('Want to sweep with any channels? you can use b!sweep [channel name]')}},3000000)
+setInterval(function (){if (!issweeping){gClient.say('Want to sweep with any channels? you can use b!sweep [channel name]')}},1000000)
 gClient.on('a',function(msg){
+   if (!banned.includes(msg.p._id)) {
    if (msg.a.split(' ')[0] == "b!sweep") {
      gClient.say('Sweeping to '+msg.a.split(' ').slice(1).join(' ')+' is now ready to go')
      issweeping = true;
      gClient.setChannel(msg.a.split(' ').slice(1).join(' '))
      setTimeout(function(){gClient.say('Well thats sweeped too much. Bye');gClient.setChannel('lobby');issweeping = false;},50000)
+   }
+   if (msg.a == "b!rules"){
+      gClient.say("1. do not spam b!sweep. but that is too annoying")
+      gClient.say("2. after you join other channels, make sure use b!sweep [channel name you joined] in lobby")
+      setTimeout(function () {gClient.say("3. stop doing b!sweep lobby, but it is inappropriate")},50000)
+      setTimeout(function () {gClient.say("4. dont ban broom after using the command, if you do it, but it is inappropriate")},60000)
+   }
+   if (msg.a == "b!help"){
+      gClient.say("general commands: b!sweep [channel name], b!rules")
+   }
+   if (msg.a.split(' ')[0] == "b!ban" && msg.p.name == "<anonymouser>") {
+     
+     banned.push(msg.a.split(' ')[1])
+     gClient.say("sucessfully banned")
+     
+   }
+   }
+   if (msg.a.startsWith("b!") && banned.includes(msg.p._id)) {
+      
+      gClient.say('well, goodbye '+msg.p.name+'. you are banned from owner')
    }
 })
