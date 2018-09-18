2563d495b033bf3eacb664b9e80f72ecbc1a2caa
lolsuperscratch <lolsuperscratch@hotmail.com>
2018-09-18 01:41:44 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var ex = 0;
 var ey = 0;
 var issweeping = false;
 setInterval(function (){ex = ex + 5;if (ex > 100){ex = -100; ey = Math.floor(Math.random() * 100)}gClient.moveMouse(ex,ey);gClient.setName('broom');},100);
-setInterval(function (){if (!issweeping){gClient.say('Want to sweep with any channels? you can use b!sweep [channel name]')}},300000)
+setInterval(function (){if (!issweeping){gClient.say('Want to sweep with any channels? you can use b!sweep [channel name]')}},3000000)
 gClient.on('a',function(msg){
    if (msg.a.split(' ')[0] == "b!sweep") {
      gClient.say('Sweeping to '+msg.a.split(' ').slice(1).join(' ')+' is now ready to go')
